4828913729046a9241a7134b67fbb76671eca55e
ferzian <m.ferzian09@gmail.com>
2025-05-12 21:56:05 +0700

    template layout

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('products');
+    return view('welcome');
 });
 Route::get('/products', function () {
     return view('products');
 });
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/product', function () {
+    return view('product');
 });
